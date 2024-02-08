5574018755ba7b8794d2ab42ba0806d381dac642
Petr Škoda <skodapetr@gmail.com>
2024-02-08 10:39:49 +0100

    Rename proposals to suggestions

--- adapter/entrypoint/proposals-list.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export PATH=/usr/local/openjdk-21/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-
-java -jar /opt/ms-adapter/ms-adapter.jar download-list --site $MS_SITE --list $MS_SUGGESTIONS_LIST --base "http://localhost/proposals" --output /data/suggestions/content.trig  >> /data/log/proposals-list.log 2>&1
