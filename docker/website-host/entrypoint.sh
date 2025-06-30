58b7e8ba8399d6216d5d6d8e11ec13689d5d898c
Petr Škoda <skodapetr@gmail.com>
2025-06-30 16:40:23 +0200

    Update website-host

--- docker/website-host/entrypoint.sh
+++ docker/website-host/entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-# Set environment variables.
-export GEM_HOME="/opt/jekyll/gems"
-export PATH="/opt/jekyll/gems/bin:$PATH"
-export RUBYOPT="-E utf-8:utf-8"
-
 # Download content.
-if [ -d "github" ]; then
+if [ -d "/var/www/html/github/" ]; then
   su - www-data -s /bin/bash -c "cd /var/www/html/github/ && git pull"
 else
   mkdir /var/www/html/github/
@@ -14,7 +9,7 @@ else
   su - www-data -s /bin/bash -c "cd /var/www/html/github/ && git clone https://github.com/$GITHUB_REPOSITORY.git ."
 fi
 
-# Compile content.
+# Optional content compilation.
 if [ "$JEKYLL_ENABLED" = "1" ]; then
   su - www-data -s /bin/bash -c "cd /var/www/html/github/ && jekyll build"
 fi
