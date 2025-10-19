7f20f4444db524225df0fdb1c181fe0b254a0e3f
Petr Škoda <skodapetr@gmail.com>
2025-10-19 16:33:28 +0200

    Update ssh-server

--- docker/ssh-server/entrypoint.sh
+++ docker/ssh-server/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Remove default user ubuntu 1000:1000 first,
+# we do not need it and can collide with new sshuser user.
+userdel ubuntu
+
 # Create a user and set a password.
 addgroup --gid $USER "sshuser"
 useradd --gid "sshuser" --uid $USER "sshuser" --create-home
