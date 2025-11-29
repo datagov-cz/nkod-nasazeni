210a67830dfc60171d9d408c4b4765ddeedf49e8
Petr Škoda <skodapetr@gmail.com>
2025-11-29 14:25:58 +0100

    Update orchestrator

--- docker/orchestrator/entrypoint/synchronize.sh
+++ docker/orchestrator/entrypoint/synchronize.sh
@@ -10,10 +10,10 @@ cd /tmp/storage/
 git clone --branch $STORAGE_REPOSITORY_BRANCH $STORAGE_REPOSITORY ./
 
 echo "Move data to storage directory"
-cp -r ./pipelines /data/lp-etl/storage/
+cp -r ./lp-etl/* /data/lp-etl/storage/
 
 echo "Remove temporary data"
-rm -rf /tmp/storage/
 
+rm -rf /tmp/storage/
 echo "Reload LinkedPipes ETL"
 curl -X POST "$STORAGE_URL/api/v1/management/reload"
