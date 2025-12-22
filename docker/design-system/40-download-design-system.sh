e2441c1914f14cbbe2f25eb6b52e114093dcedc2
Petr Škoda <skodapetr@gmail.com>
2025-12-22 13:49:08 +0100

    Update design-system
    Fix script, move content to design-system directory.

--- docker/design-system/40-download-design-system.sh
+++ docker/design-system/40-download-design-system.sh
@@ -3,6 +3,8 @@ if [ ! -d "/usr/share/nginx/html/design-system/" ]; then
   cd /tmp
   wget https://github.com/datagov-cz/design-system/releases/download/2025-07-01/design-system.zip
   unzip design-system.zip
-  # Create a target and copy.
-  mv assets/design-system/* /usr/share/nginx/html/
+  # Create the target directory.
+  mkdir /usr/share/nginx/html/design-system/
+  # Copy content.
+  mv assets/design-system/* /usr/share/nginx/html/design-system/
 fi
